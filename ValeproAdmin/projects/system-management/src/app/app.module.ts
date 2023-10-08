23afb55e98f99e21bcfda26446e5df669ddc1703
Rafael Ramos <rafael.ramosr@hotmail.com>
2023-10-08 11:51:01 -0500

    feat: system management module wit routing

--- ValeproAdmin/projects/system-management/src/app/app.module.ts
+++ ValeproAdmin/projects/system-management/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SystemManagementModule } from './system-management/system-management.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, SystemManagementModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ ValeproAdmin/projects/system-management/src/app/system-management/system-management.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SystemManagementRoutingModule } from './system-management-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SystemManagementRoutingModule
+  ]
+})
+export class SystemManagementModule { }
