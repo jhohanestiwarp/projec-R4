6c85006fbd4f57f5cab187384983ac0c9c91256f
Rafael Ramos <rafael.ramosr@hotmail.com>
2023-10-08 11:52:03 -0500

    chore: system management webpack config

--- ValeproAdmin/projects/system-management/webpack.config.js
+++ ValeproAdmin/projects/system-management/webpack.config.js
@@ -1,58 +1,21 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, '../../tsconfig.json'),
-  [/* mapped paths to share */]);
+module.exports = withModuleFederationPlugin({
+  name: "System",
 
-module.exports = {
-  output: {
-    uniqueName: "systemManagement",
-    publicPath: "auto"
+  exposes: {
+    "./SystemManagementModule":
+      "projects/system-management/src/app/system-management/system-management.module.ts",
   },
-  optimization: {
-    runtimeChunk: false
-  },   
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
-  },
-  experiments: {
-    outputModule: true
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-        library: { type: "module" },
 
-        // For remotes (please adjust)
-        // name: "systemManagement",
-        // filename: "remoteEntry.js",
-        // exposes: {
-        //     './Component': './projects/system-management/src/app/app.component.ts',
-        // },        
-        
-        // For hosts (please adjust)
-        // remotes: {
-        //     "administrator": "http://localhost:4200/remoteEntry.js",
-        //     "authorizer": "http://localhost:4201/remoteEntry.js",
-        //     "programManagement": "http://localhost:4202/remoteEntry.js",
-
-        // },
-
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-
-          ...sharedMappings.getDescriptors()
-        })
-        
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
     }),
-    sharedMappings.getPlugin()
-  ],
-};
+  },
+});
