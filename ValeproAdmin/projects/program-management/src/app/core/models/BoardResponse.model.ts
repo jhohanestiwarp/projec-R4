4a7697c40ddce168c24df6f59e5a2e840dfcc0d6
Rafael Ramos <rafael.ramosr@hotmail.com>
2023-10-14 14:06:50 -0500

    feat: add request and response models

---
+++ ValeproAdmin/projects/program-management/src/app/core/models/BoardResponse.model.ts
@@ -0,0 +1,28 @@
+export class BoardCreateResponse {
+  constructor(public CreateBoardStatus: boolean) {}
+}
+
+export class BoardRemoveResponse {
+  constructor(public DeleteBoardStatus: boolean) {}
+}
+
+export class BoardByTypeAndProgramResponse {
+  constructor(
+    public BoardId: number,
+    public BoardTypeId: number,
+    public LanguageId: number,
+    public ProgramId: number,
+    public Name: string,
+    public StartDateValidity: string,
+    public EndDateValidity: string,
+    public OpeningModeId: number,
+    public Url: string,
+    public Image: string,
+    public DisplayOrder: number,
+    public Properties: string,
+    public DateRegister: string,
+    public DateUpdate: string,
+    public PersonIdCreate: number,
+    public PersonIdUpdate: number
+  ) {}
+}
