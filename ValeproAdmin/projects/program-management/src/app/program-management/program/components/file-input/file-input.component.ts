113e39acdb3571c8b129856fff13f1155093d877
Rafael Ramos <rafael.ramosr@hotmail.com>
2023-10-15 09:24:35 -0500

    fix: not show filename

--- ValeproAdmin/projects/program-management/src/app/program-management/program/components/file-input/file-input.component.ts
+++ ValeproAdmin/projects/program-management/src/app/program-management/program/components/file-input/file-input.component.ts
@@ -6,8 +6,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./file-input.component.scss'],
 })
 export class FileInputComponent {
-  filename = '';
-
   @Input()
   fileTitle = '';
   @Input()
@@ -21,20 +19,7 @@ export class FileInputComponent {
   onFileSelect(ev: any): void {
     const fileList: FileList = ev?.target?.files;
     if (fileList && fileList.length) {
-      this.filename = this.setFilename(fileList);
       this.uploadFile.emit(fileList);
     }
-  }
-
-  onRemove(): void {
-    this.filename = '';
-  }
-
-  private setFilename(files: FileList): string {
-    if (files.length === 1) {
-      return files[0].name;
-    }
-
-    return `${files.length} archivos.`;
   }
 }
