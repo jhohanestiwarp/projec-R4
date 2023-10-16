a128b13aa4b7630b4143e3880064f888f50f2412
Rafael Ramos <rafael.ramosr@hotmail.com>
2023-10-16 06:47:31 -0500

    fix: remove unused project

--- ValeproAdmin/projects/system-management/src/app/system-management/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-export { URFormStepBaseComponent } from './ur-form-base/ur-form-base.component';
