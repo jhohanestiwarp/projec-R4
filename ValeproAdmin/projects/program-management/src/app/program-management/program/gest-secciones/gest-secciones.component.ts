22d55d8956d5cf12e96a2fdd735ea98eb1de1558
Rafael Ramos <rafael.ramosr@hotmail.com>
2023-10-15 21:33:45 -0500

    feat: add modal confirm and alerts

--- ValeproAdmin/projects/program-management/src/app/program-management/program/gest-secciones/gest-secciones.component.ts
+++ ValeproAdmin/projects/program-management/src/app/program-management/program/gest-secciones/gest-secciones.component.ts
@@ -1,11 +1,11 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 
-import { ModalComponent } from '../components';
+import { AlertModalService } from '../../../infrastructure/services/alertModal.service';
 import { ProgramSectionsRepository } from '../../../core/repositories/program-sections.repository';
+import { IndividualConfig, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-gest-secciones',
@@ -38,6 +38,17 @@ export class GestSeccionesComponent {
     },
   };
 
+  // config
+  private readonly toastConfig: Partial<IndividualConfig> = {
+    timeOut: 9000,
+    progressBar: true,
+    disableTimeOut: 'extendedTimeOut',
+    progressAnimation: 'increasing',
+    tapToDismiss: false,
+    positionClass: 'toast-top-center',
+    closeButton: true,
+  };
+
   // selectors
   openingOptions = [
     { label: 'Abrir desde la misma pestaña', value: 1 },
@@ -60,41 +71,68 @@ export class GestSeccionesComponent {
   $loading: Observable<boolean> = new Observable();
   managementForm: FormGroup = this.fb.group({
     endDate: ['', Validators.required],
-    file: [[]],
+    file: [null],
     opening: [this.openingOptions[0].value],
-    segment: ['', Validators.required],
+    segment: [[], Validators.required],
     section: ['', Validators.required],
     startDate: ['', Validators.required],
     title: ['', [Validators.maxLength(50), Validators.required]],
     url: '',
-    segmentCheckbox: false,
   });
 
-  animal: string = '';
-  name: string = '';
-
   constructor(
+    private alertModalService: AlertModalService,
     private fb: FormBuilder,
-    private dialog: MatDialog,
     private programSectionsRepository: ProgramSectionsRepository,
-    // private toastrService: ToastrService,
+    private toastrService: ToastrService
   ) {}
 
-  addBoard() {
+  async addBoard() {
+    if (this.managementForm.invalid) {
+      this.toastrService.error(
+        'Por favor, llene los campos requeridos.',
+        undefined,
+        this.toastConfig
+      );
+      return;
+    }
+
+    let imageData = '';
+    const { file, url } = this.managementForm.value;
+
+    if (file) {
+      imageData = await this.fileToBase64(file);
+
+      if (!imageData) {
+        this.toastrService.error(
+          'Error al procesar la imagen.',
+          undefined,
+          this.toastConfig
+        );
+        return;
+      }
+    }
+
+    const truncatedUrl = this.truncateUrl(url || 'N/A');
+    // temporal push
+    this.tableData.push({
+      position: this.tableData.length + 1,
+      url: truncatedUrl,
+    });
     const request = this.programSectionsRepository.createBoard({
-      BoardTypeId: 1,
-      Segments: [1],
+      BoardTypeId: this.managementForm.value.section,
+      Segments: this.managementForm.value.segment,
       LanguageId: 1,
       ProgramId: 1,
-      Name: '',
-      StartDateValidity: '',
-      EndDateValidity: '',
-      OpeningModeId: 1,
-      Url: '',
+      Name: this.managementForm.value.title,
+      StartDateValidity: this.managementForm.value.startDate,
+      EndDateValidity: this.managementForm.value.endDate,
+      OpeningModeId: this.managementForm.value.opening,
+      Url: truncatedUrl,
       Image: {
-        ImageData: '',
-        ImageName: '',
-        ImageExtension: '',
+        ImageData: imageData,
+        ImageName: file?.name,
+        ImageExtension: file?.name.split('.').pop(),
       },
       Properties: '',
     });
@@ -102,29 +140,31 @@ export class GestSeccionesComponent {
     request.subscribe({
       next: (res) => {
         if (!res.CreateBoardStatus) {
-          // informar al usuario que no se creó
+          this.toastrService.error(
+            'Los datos no se guardaron correctamente.',
+            undefined,
+            this.toastConfig
+          );
           return;
         }
 
-        // informar al usuario que se creó
+        this.toastrService.success(
+          'Archivo subido con éxito.',
+          undefined,
+          this.toastConfig
+        );
       },
-      error: () => {},
-    });
-  }
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(ModalComponent, {
-      data: { name: this.name, animal: this.animal },
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.animal = result;
+      error: () => {
+        this.toastrService.error(
+          'Ocurrió un error durante la operación.',
+          undefined,
+          this.toastConfig
+        );
+      },
     });
   }
 
   getFiles(fileList: FileList): void {
-    fileList[0].name
     this.managementForm.patchValue({
       ...this.managementForm.value,
       file: fileList[0],
@@ -146,28 +186,17 @@ export class GestSeccionesComponent {
     moveItemInArray(this.tableData, event.previousIndex, event.currentIndex);
   }
 
-  onDelete(position: number): void {
-    const dialogRef = this.dialog.open(ModalComponent, {});
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === 'confirm') {
-        this.tableData = this.tableData.filter(item => item.position !== position);
-      }
+  async removeItem(position: number) {
+    const isConfirm = await this.alertModalService.warningAlert({
+      title: '¿Estás seguro de eliminar este board?',
+      accept: 'Eliminar',
+      cancel: 'Volver a secciones',
     });
-  }
 
- onSubmit(): void {
-  if (this.managementForm.valid) {
-    const { section, title, url } = this.managementForm.value;
-    const truncatedUrl = this.truncateUrl(url || 'N/A');
-    this.tableData.push({ position: this.tableData.length + 1, url: truncatedUrl });
-    // Asigna la URL truncada al formulario antes de agregarla a tableData
-    this.managementForm.patchValue({
-      ...this.managementForm.value,
-      url: truncatedUrl
-    });
-    this.managementForm.reset();
+    if (!isConfirm) return;
+
+    this.tableData = this.tableData.filter((e) => e.position !== position);
   }
-}
 
   validateStartDate(ev: any, field: 'endDate' | 'startDate') {
     const regex = /[^0-9/]/;
@@ -185,5 +214,20 @@ export class GestSeccionesComponent {
     if (value.length === 10 && !isNaN(parsedDate)) {
       this.managementForm.get(field)?.patchValue(new Date(parsedDate));
     }
+  }
+
+  private fileToBase64(file: File): Promise<string> {
+    const reader = new FileReader();
+
+    return new Promise((resolve, _reject) => {
+      reader.onload = (ev: any) => {
+        const base64String = ev.target.result;
+
+        return resolve(base64String);
+      };
+
+      reader.onerror = () => resolve('');
+      reader.readAsDataURL(file);
+    });
   }
 }
