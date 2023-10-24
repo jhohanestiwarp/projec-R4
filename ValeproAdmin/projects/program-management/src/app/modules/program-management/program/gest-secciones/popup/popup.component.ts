a774019dcdada8b0283a0ddec720c5bf0b50f407
Rafael Ramos <rafael.ramos@energiasolarsa.com>
2023-10-24 09:22:19 -0500

    feat: use board service in popup and widgets

--- ValeproAdmin/projects/program-management/src/app/modules/program-management/program/gest-secciones/popup/popup.component.ts
+++ ValeproAdmin/projects/program-management/src/app/modules/program-management/program/gest-secciones/popup/popup.component.ts
@@ -2,9 +2,11 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DialogParams, DialogService } from 'projects/dialogs-lib/src/public-api';
+import { BoardRepository } from 'projects/program-management/src/app/core/repositories/board.repository';
+import { IndividualConfig, ToastrService } from 'ngx-toastr';
 
 interface Parts {
-  value: string;
+  value: number;
   viewValue: string;
 }
 
@@ -18,19 +20,31 @@ export class PopupComponent {
 
 
   //#region variables
+  file!: File;
   popupSelect!: FormGroup;
   selectedSegmento?: string;
   currentDate: Date = new Date();
+  private readonly toastConfig: Partial<IndividualConfig<any>> = {
+    timeOut: 9000,
+    progressBar: true,
+    disableTimeOut: 'extendedTimeOut',
+    progressAnimation: 'increasing',
+    tapToDismiss: false,
+    positionClass: 'toast-top-center',
+    closeButton: true,
+  };
   //#endregion
 
   //#region Injectables
+  private boardService: BoardRepository = inject(BoardRepository);
+  private toastService: ToastrService = inject(ToastrService);
   dialogService = inject(DialogService);
   //#endregion
 
   Segmentos: Parts[] = [
-    { value: 'Option 1', viewValue: 'Option 1' },
-    { value: 'Option 2', viewValue: 'Option 2' },
-    { value: 'Option 3', viewValue: 'Option 3' },
+    { value: 1, viewValue: 'Option 1' },
+    { value: 2, viewValue: 'Option 2' },
+    { value: 3, viewValue: 'Option 3' },
   ];
 
 
@@ -44,14 +58,99 @@ export class PopupComponent {
     });
   }
 
-  submit() {
-    this.openPopUp();
+
+  async submit() {
+    const imageData = await this.fileToBase64(this.file);
+    const imageName = this.file.name;
+    const imageExtension = imageName.split('.').pop();
+    const boardTypeId = 1;
+    const programId= 1;
+
+    this.boardService.boardCreate({
+      languageId: 1,
+      openingModeId: 1,
+      boardTypeId,
+      programId,
+      segments: this.popupSelect.value.segmento,
+      name: this.popupSelect.value.titulo,
+      startDateValidity: this.popupSelect.value.starDate,
+      endDateValidity: this.popupSelect.value.endDate,
+      url: this.popupSelect.value.url,
+      image: {
+        imageData,
+        imageName,
+        imageExtension: `.${imageExtension}`,
+      },
+      properties: null,
+    }).subscribe({
+      next: (res) => {
+        if (res.codeId !== 200) {
+          this.toastService.error(res.message, undefined, this.toastConfig);
+          return;
+        }
+
+        this.openPopUp();
+        this.listBoard(boardTypeId, programId);
+      },
+      error: () => this.unexpectedError()
+    })
   }
 
-  openPopUp() {
+  removeItem(boardId: number) {
+    this.boardService.boardDelete({ boardId }).subscribe({
+      next: (res) => {
+        if (res.codeId !== 200 || res.data.deleteBoardStatus === false) {
+          this.toastService.error(res.message, undefined, this.toastConfig);
+          return;
+        }
+
+        this.openPopUp('¡Registro eliminado con éxito!');
+        const programId = 1;
+        this.listBoard(boardId, programId)
+      },
+      error: () => this.unexpectedError()
+    });
+  }
+
+  listBoard(boardTypeId: number, programId: number) {
+    this.boardService.getBoardByTypeAndProgram({
+      boardTypeId,
+      programId,
+    }).subscribe({
+      next: (res) => {
+        if (res.codeId !== 200) {
+          this.toastService.error(res.message, undefined, this.toastConfig);
+          return;
+        }
+
+        // asignar al listado
+        res.data.boardEntities;
+      },
+      error: () => this.unexpectedError()
+    })
+  }
+
+  private fileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = reject;
+    });
+  }
+
+  private unexpectedError() {
+    this.toastService.error(
+      'Lo sentimos, ha sucedido un error inesperado.',
+      undefined,
+      this.toastConfig
+    );
+  }
+
+  openPopUp(title: string = '¡Cambios guardados con éxito!') {
     let params: DialogParams = {
       success: false,
-      title: '¡Cambios guardados con éxito!',
+      title,
       confirmText: ''
     };
     this.dialogService.openConfirmDialog(params);
--- ValeproAdmin/projects/program-management/src/app/modules/program-management/program/gest-secciones/widgets/widgets.component.ts
+++ ValeproAdmin/projects/program-management/src/app/modules/program-management/program/gest-secciones/widgets/widgets.component.ts
@@ -2,9 +2,11 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DialogParams, DialogService } from 'projects/dialogs-lib/src/public-api';
+import { BoardRepository } from 'projects/program-management/src/app/core/repositories/board.repository';
+import { IndividualConfig, ToastrService } from 'ngx-toastr';
 
 interface Parts {
-  value: string;
+  value: number;
   viewValue: string;
 }
 
@@ -17,21 +19,32 @@ interface Parts {
 export class WidgetsComponent {
 
   //#region variables
+  file!: File;
   widgetSelect!: FormGroup;
   selectedSegmento?: string;
   currentDate: Date = new Date();
-
+  private readonly toastConfig: Partial<IndividualConfig<any>> = {
+    timeOut: 9000,
+    progressBar: true,
+    disableTimeOut: 'extendedTimeOut',
+    progressAnimation: 'increasing',
+    tapToDismiss: false,
+    positionClass: 'toast-top-center',
+    closeButton: true,
+  };
   //#endregion
 
   //#region Injectables
+  private boardService: BoardRepository = inject(BoardRepository);
+  private toastService: ToastrService = inject(ToastrService);
   dialogService = inject(DialogService);
   //#endregion
 
 
   Segmentos: Parts[] = [
-    { value: 'Option 1', viewValue: 'Option 1' },
-    { value: 'Option 2', viewValue: 'Option 2' },
-    { value: 'Option 3', viewValue: 'Option 3' },
+    { value: 1, viewValue: 'Option 1' },
+    { value: 2, viewValue: 'Option 2' },
+    { value: 3, viewValue: 'Option 3' },
   ];
 
 
@@ -47,14 +60,98 @@ export class WidgetsComponent {
     });
   }
 
-  submit() {
-    this.openPopUp();
+  async submit() {
+    const imageData = await this.fileToBase64(this.file);
+    const imageName = this.file.name;
+    const imageExtension = imageName.split('.').pop();
+    const boardTypeId = 1;
+    const programId= 1;
+
+    this.boardService.boardCreate({
+      languageId: 1,
+      openingModeId: 1,
+      boardTypeId,
+      programId,
+      segments: this.widgetSelect.value.segmento,
+      name: this.widgetSelect.value.titulo,
+      startDateValidity: this.widgetSelect.value.starDate,
+      endDateValidity: this.widgetSelect.value.endDate,
+      url: this.widgetSelect.value.url,
+      image: {
+        imageData,
+        imageName,
+        imageExtension: `.${imageExtension}`,
+      },
+      properties: null,
+    }).subscribe({
+      next: (res) => {
+        if (res.codeId !== 200) {
+          this.toastService.error(res.message, undefined, this.toastConfig);
+          return;
+        }
+
+        this.openPopUp();
+        this.listBoard(boardTypeId, programId);
+      },
+      error: () => this.unexpectedError()
+    })
   }
 
-  openPopUp() {
+  removeItem(boardId: number) {
+    this.boardService.boardDelete({ boardId }).subscribe({
+      next: (res) => {
+        if (res.codeId !== 200 || res.data.deleteBoardStatus === false) {
+          this.toastService.error(res.message, undefined, this.toastConfig);
+          return;
+        }
+
+        this.openPopUp('¡Registro eliminado con éxito!');
+        const programId = 1;
+        this.listBoard(boardId, programId)
+      },
+      error: () => this.unexpectedError()
+    });
+  }
+
+  listBoard(boardTypeId: number, programId: number) {
+    this.boardService.getBoardByTypeAndProgram({
+      boardTypeId,
+      programId,
+    }).subscribe({
+      next: (res) => {
+        if (res.codeId !== 200) {
+          this.toastService.error(res.message, undefined, this.toastConfig);
+          return;
+        }
+
+        // asignar al listado
+        res.data.boardEntities;
+      },
+      error: () => this.unexpectedError()
+    })
+  }
+
+  private fileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = reject;
+    });
+  }
+
+  private unexpectedError() {
+    this.toastService.error(
+      'Lo sentimos, ha sucedido un error inesperado.',
+      undefined,
+      this.toastConfig
+    );
+  }
+
+  openPopUp(title: string = '¡Cambios guardados con éxito!') {
     let params: DialogParams = {
       success: false,
-      title: '¡Cambios guardados con éxito!',
+      title,
       confirmText: ''
     };
     this.dialogService.openConfirmDialog(params);
